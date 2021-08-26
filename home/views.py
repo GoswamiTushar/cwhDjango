9f0fa21d203b58e34a470a30b4c632367735d9f7
GoswamiTushar <tushargoswami1510@gmail.com>
2021-08-26 09:27:59 +0530

    Test

---
+++ home/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    # return HttpResponse("This is Homepage")
+    context = {"var2": "Django app", "variable": "Test paragraph"}
+    return render(request, 'index.html', context)
+
+
+def about(request):
+    return render(request, 'about.html')
+    # return HttpResponse("This is about page")
+
+
+def services(request):
+    return render(request, 'services.html')
+    # return HttpResponse("This is services page")
+
+
+def contact(request):
+    return render(request, 'contact.html')
+    # return HttpResponse("This is Contact page")
