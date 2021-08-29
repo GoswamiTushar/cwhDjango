f7fc95ea358f9956b3df0c639e591e6b4d4df05f
GoswamiTushar <tushargoswami1510@gmail.com>
2021-08-29 22:36:40 +0530

    feat: form functionality added

--- home/urls.py
+++ home/urls.py
@@ -22,5 +22,4 @@ urlpatterns = [
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
     path('services', views.services, name='services'),
-
 ]
--- home/views.py
+++ home/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, HttpResponse
-
+from datetime import datetime
+from home.models import Contact
 # Create your views here.
 
 
 def index(request):
     # return HttpResponse("This is Homepage")
-    context = {"var2": "Django app", "variable": "Test paragraph"}
+    context = {"var2": "The Sneaker Store", "variable": "Test paragraph"}
     return render(request, 'index.html', context)
 
 
@@ -20,5 +21,13 @@ def services(request):
 
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        number = request.POST.get("number")
+        desc = request.POST.get("desc")
+        contact = Contact(name=name, email=email, number=number,
+                          desc=desc, date=datetime.today())
+        contact.save()
     return render(request, 'contact.html')
     # return HttpResponse("This is Contact page")
